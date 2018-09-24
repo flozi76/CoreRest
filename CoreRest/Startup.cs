9873cddb841a20524ca1014f9bedf85a230ac2d1
Florian Zimmermann <flozi76@bluewin.ch>
2018-09-24 21:51:01 +0200

    rest docker srv

--- CoreRest/Startup.cs
+++ CoreRest/Startup.cs
@@ -5,14 +5,24 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
-namespace CoreRest
+namespace aspnetapp
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IHostingEnvironment env)
+        {
+            // var builder = new ConfigurationBuilder()
+            //     .SetBasePath(env.ContentRootPath)
+            //     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            //     .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+            //     .AddEnvironmentVariables();
+            // Configuration = builder.Build();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
         }
@@ -30,5 +40,40 @@ namespace CoreRest
                 await context.Response.WriteAsync("Hello World! T");
             });
         }
+
+        // public IConfigurationRoot Configuration { get; }
+
+        // // This method gets called by the runtime. Use this method to add services to the container.
+        // public void ConfigureServices(IServiceCollection services)
+        // {
+        //     // Add framework services.
+        //     services.AddMvc();
+        // }
+
+        // // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        // {
+        //     loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+        //     loggerFactory.AddDebug();
+
+        //     if (env.IsDevelopment())
+        //     {
+        //         app.UseDeveloperExceptionPage();
+        //         app.UseBrowserLink();
+        //     }
+        //     else
+        //     {
+        //         app.UseExceptionHandler("/Home/Error");
+        //     }
+
+        //     app.UseStaticFiles();
+
+        //     app.UseMvc(routes =>
+        //     {
+        //         routes.MapRoute(
+        //             name: "default",
+        //             template: "{controller=Home}/{action=Index}/{id?}");
+        //     });
+        // }
     }
 }
